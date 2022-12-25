0e88c8a186c091099628faab92fbe4ac9f4762a5
YuriiKabo <yourrocksmirnov@gmail.com>
2022-12-26 01:50:01 +0200

    dev lift fin

--- src/js/index.js
+++ src/js/index.js
@@ -1,50 +1,89 @@
 const refs = {
   body: document.querySelector('body'),
+  floors: document.querySelector('.floors'),
   lift: document.querySelector('[lift]'),
   btnCall: document.querySelectorAll('.button__call'),
+  liftBtn: document.querySelectorAll('.lift-button'),
   tabNum: document.querySelectorAll('.tab__quad-num'),
+  doors: document.querySelectorAll('.outdoor'),
 };
 
-const ACTIVE_COLOR = '#c156ff';
-const INACTIVE_COLOR = '#e6e6e6';
+const ACTIVE_COLOR = '#ff3535';
+const INACTIVE_COLOR = '#efefef';
 const DOORS_TIMEOUT = 4000;
 const LIFT_SPEED = 7;
 let LIFT_DELAY = 0;
+let doorNumb = 1;
 
-refs.body.addEventListener('focusin', onLiftCall);
+refs.body.addEventListener('click', onLiftActiveate);
 
-function onLiftCall(e) {
-  let liftCurrentPosition = parseInt(refs.lift.style.top);
-  let whereLiftBecome = parseInt(refs.lift.style.top);
-  if (e.target.id === 'f-1-btn') {
-    whereLiftBecome = 844;
-  } else if (e.target.id === 'f-2-btn') {
-    whereLiftBecome = 644;
-  } else if (e.target.id === 'f-3-btn') {
-    whereLiftBecome = 444;
-  } else if (e.target.id === 'f-4-btn') {
-    whereLiftBecome = 244;
-  } else if (e.target.id === 'f-5-btn') {
-    whereLiftBecome = 44;
+function onLiftActiveate(e) {
+  if (e.target.nodeName === 'BUTTON') {
+    const liftCurrentPosition = parseInt(refs.lift.style.top);
+    let whereLiftBecome = parseInt(refs.lift.style.top);
+    if (e.target.id === 'f1' || e.target.id === 'f-1-btn') {
+      whereLiftBecome = 844;
+    } else if (e.target.id === 'f2' || e.target.id === 'f-2-btn') {
+      whereLiftBecome = 644;
+    } else if (e.target.id === 'f3' || e.target.id === 'f-3-btn') {
+      whereLiftBecome = 444;
+    } else if (e.target.id === 'f4' || e.target.id === 'f-4-btn') {
+      whereLiftBecome = 244;
+    } else if (e.target.id === 'f5' || e.target.id === 'f-5-btn') {
+      whereLiftBecome = 44;
+    }
+    if (e.target.className === 'lift-button') {
+      closeDoorsMomentaly();
+      LIFT_DELAY = 2000;
+    }
+    setTimeout(() => {
+      whereSendLift(e, liftCurrentPosition, whereLiftBecome);
+    }, LIFT_DELAY);
+    activateLiftBtn(e);
+    if (e.target.className === 'button__call') {
+      e.target.style.backgroundColor = '#88ff3e';
+    }
   }
-  setTimeout(() => {
-    onActivateLift(e, liftCurrentPosition, whereLiftBecome);
-  }, LIFT_DELAY);
-  activeCallBtns(e, liftCurrentPosition, whereLiftBecome);
 }
 
-function onActivateLift(e, liftCurrentPosition, whereLiftBecome) {
+function whereSendLift(e, liftCurrentPosition, whereLiftBecome) {
   if (liftCurrentPosition > whereLiftBecome) {
     const plus = '1';
     liftToGo(e, liftCurrentPosition, whereLiftBecome, plus);
-    tabCounter(liftCurrentPosition, whereLiftBecome, plus);
+    floorCounter(liftCurrentPosition, whereLiftBecome, plus);
   } else if (liftCurrentPosition < whereLiftBecome) {
     const minus = '-1';
     liftToGo(e, liftCurrentPosition, whereLiftBecome, minus);
-    tabCounter(liftCurrentPosition, whereLiftBecome, minus);
-  } else if ((liftCurrentPosition = whereLiftBecome)) {
-    onOpenDoors(e);
+    floorCounter(liftCurrentPosition, whereLiftBecome, minus);
+  } else if (liftCurrentPosition === whereLiftBecome) {
+    activateLiftBtn(e);
+    setTimeout(() => {
+      inActiveLiftBtn(e);
+    }, 500);
+    onToggleDoor(doorNumb);
   }
+}
+
+function floorCounter(liftCurrentPosition, whereLiftBecome, op) {
+  const counterId = setInterval(() => {
+    let num = liftCurrentPosition - `${op}`;
+    liftCurrentPosition = num;
+    if (844 > liftCurrentPosition && 744 < liftCurrentPosition) {
+      doorNumb = 1;
+    } else if (744 > liftCurrentPosition && 544 < liftCurrentPosition) {
+      doorNumb = 2;
+    } else if (544 > liftCurrentPosition && 344 < liftCurrentPosition) {
+      doorNumb = 3;
+    } else if (344 > liftCurrentPosition && 144 < liftCurrentPosition) {
+      doorNumb = 4;
+    } else if (144 > liftCurrentPosition && 44 < liftCurrentPosition) {
+      doorNumb = 5;
+    }
+    if (liftCurrentPosition === whereLiftBecome) {
+      clearInterval(counterId);
+    }
+    setNumberOfFloor(doorNumb);
+  }, LIFT_SPEED);
 }
 
 function liftToGo(e, liftCurrentPosition, whereLiftBecome, op) {
@@ -53,48 +92,94 @@ function liftToGo(e, liftCurrentPosition, whereLiftBecome, op) {
     liftCurrentPosition = val;
     refs.lift.style.top = `${liftCurrentPosition}px`;
     if (liftCurrentPosition === whereLiftBecome) {
+      onOpenDoor(doorNumb);
       clearInterval(intervalId);
-      onLiftWaiting();
-      inActiveCallBtns(e);
-      // refs.lift.style.zIndex = 1;
-      e.target.previousElementSibling.classList.add('outdoor-open');
+      if (e.target.className === 'button__call') {
+        onLiftWaiting();
+      }
+      inActiveLiftBtn(e);
       setTimeout(() => {
         LIFT_DELAY = 0;
-        e.target.previousElementSibling.classList.remove('outdoor-open');
+        closeDoorsMomentaly();
       }, DOORS_TIMEOUT);
     }
   }, LIFT_SPEED);
 }
 
-function tabCounter(liftCurrentPosition, whereLiftBecome, op) {
-  const counterId = setInterval(() => {
-    let num = liftCurrentPosition - `${op}`;
-    liftCurrentPosition = num;
-    if (844 > liftCurrentPosition && 744 < liftCurrentPosition) {
-      refs.tabNum.forEach(floorNum => {
-        floorNum.textContent = 1;
+function onToggleDoor(doorNumb) {
+  closeDoorsTimeout();
+  for (let i = 0; i < refs.tabNum.length; i += 1) {
+    if (i === 0) {
+      refs.doors.forEach(door => {
+        const numOfFloor = doorNumb;
+        if (door.id === `d${numOfFloor}`) {
+          door.classList.toggle('outdoor-open');
+        }
       });
-    } else if (744 > liftCurrentPosition && 544 < liftCurrentPosition) {
-      refs.tabNum.forEach(floorNum => {
-        floorNum.textContent = 2;
-      });
-    } else if (544 > liftCurrentPosition && 344 < liftCurrentPosition) {
-      refs.tabNum.forEach(floorNum => {
-        floorNum.textContent = 3;
-      });
-    } else if (344 > liftCurrentPosition && 144 < liftCurrentPosition) {
-      refs.tabNum.forEach(floorNum => {
-        floorNum.textContent = 4;
-      });
-    } else if (144 > liftCurrentPosition && 44 < liftCurrentPosition) {
-      refs.tabNum.forEach(floorNum => {
-        floorNum.textContent = 5;
-      });
+      break;
     }
-    if (liftCurrentPosition === whereLiftBecome) {
-      clearInterval(counterId);
+  }
+}
+
+function onOpenDoor(doorNumb) {
+  closeDoorsTimeout();
+  for (let i = 0; i < refs.tabNum.length; i += 1) {
+    if (i === 0) {
+      refs.doors.forEach(door => {
+        const numOfFloor = doorNumb;
+        if (door.id === `d${numOfFloor}`) {
+          door.classList.add('outdoor-open');
+        }
+      });
+      break;
     }
-  }, LIFT_SPEED);
+  }
+}
+
+function closeDoorsTimeout() {
+  setTimeout(() => {
+    closeDoorsMomentaly();
+  }, DOORS_TIMEOUT);
+}
+
+function closeDoorsMomentaly() {
+  refs.doors.forEach(door => {
+    door.classList.remove('outdoor-open');
+  });
+}
+
+function activateLiftBtn(e) {
+  e.target.style.backgroundColor = '#3f86ff';
+  e.target.style.color = '#ffffff';
+  refs.lift.removeEventListener('click', onLiftActiveate);
+  activeCallBtns();
+}
+
+function inActiveLiftBtn(e) {
+  e.target.style.backgroundColor = INACTIVE_COLOR;
+  e.target.style.color = '#000000';
+  refs.lift.addEventListener('click', onLiftActiveate);
+  inActiveCallBtns();
+}
+
+function activeCallBtns() {
+  refs.btnCall.forEach(btn => {
+    btn.disabled = true;
+    btn.style.backgroundColor = ACTIVE_COLOR;
+  });
+}
+
+function inActiveCallBtns() {
+  refs.btnCall.forEach(btn => {
+    btn.disabled = false;
+    btn.style.backgroundColor = INACTIVE_COLOR;
+  });
+}
+
+function setNumberOfFloor(doorNumb) {
+  refs.tabNum.forEach(floorNum => {
+    floorNum.textContent = doorNumb;
+  });
 }
 
 function onLiftWaiting() {
@@ -107,33 +192,4 @@ function onLiftWaiting() {
       clearInterval(liftInterval);
     }
   }, 100);
-}
-
-function onOpenDoors(e) {
-  e.target.previousElementSibling.classList.toggle('outdoor-open');
-  onCloseDoors(e);
-  setTimeout(() => {
-    inActiveCallBtns();
-  }, 500);
-}
-
-function onCloseDoors(e) {
-  setTimeout(() => {
-    e.target.previousElementSibling.classList.remove('outdoor-open');
-  }, DOORS_TIMEOUT);
-}
-
-function activeCallBtns(e) {
-  refs.btnCall.forEach(btn => {
-    btn.disabled = true;
-    btn.style.backgroundColor = ACTIVE_COLOR;
-  });
-  e.target.style.backgroundColor = '#ff5689';
-}
-
-function inActiveCallBtns() {
-  refs.btnCall.forEach(btn => {
-    btn.disabled = false;
-    btn.style.backgroundColor = INACTIVE_COLOR;
-  });
 }
